7c00fcc0c5c9a8299d94d2b040da487ac4d504e6
Vitu07 <vitormorais20092004@gmail.com>
2025-05-10 15:54:41 -0300

    add migrations

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202505101532.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+--changeSet morais:202505101406
+--comment: cards table create
+
+CREATE TABLE CARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    `order` int NOT NULL,
+    board_column_id BIGINT NOT NULL,
+    CONSTRAINT boards_columns__cards_fk FOREIGN KEY(board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
+
+
+) ENGINE=InnoDB;
+
+--rollback: DROP TABLE CARDS
